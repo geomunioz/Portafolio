f262b5cfd2734a21c18b859dfb351c622cc9300a
jorgetmz@outlook.com <jorgetmz@outlook.com>
2021-05-22 20:24:21 -0500

    Portafolio Personal

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 50){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    //slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+});
